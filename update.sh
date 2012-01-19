a512c834e0aeb1972f70f62c9211bc4070d88455
Vladimir Epifanov <voldmar@voldmar.ru>
2012-01-19 13:01:37 +0400

    Verbose mode + Vundle update

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 cd $HOME/.vim
-git pull > /dev/null
-git submodule init > /dev/null
-git submodule update > /dev/null
+
+git pull
+
+# Update vundle as it only submodule
+git submodule init
+git submodule update
+
+# Some service dirs
+[ -d tmp ] || mkdir -p tmp/backup tmp/swap tmp/undo
+
+# Let’s Vundle update all bundles
+vim -c ':BundleInstall!' -c ':q' -c ':q'
 
