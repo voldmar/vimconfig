240bfaac1c50dc16cc9e645f784e3638f5a52661
Vladimir Epifanov <voldmar@voldmar.ru>
2012-08-21 10:18:06 +0400

    util scripts refactoring

--- update.sh
+++ update.sh
@@ -4,9 +4,6 @@ cd $HOME/.vim
 
 git pull --rebase
 
-# Some service dirs
-[ -d tmp ] || mkdir -p tmp/backup tmp/swap tmp/undo
-
 # Let’s Vundle update all bundles
 vim +BundleClean\! +qall
 vim +BundleInstall\! +qall
